fc77053b9f795c2ea07c0737aa33b34dadac04e3
Vince Rono <Vince Rono>
2023-02-21 12:51:36 -0800

    task 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the letters of the alphabet
+ * Description: VR
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char let = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+{
+_putchar('\n')
+}
