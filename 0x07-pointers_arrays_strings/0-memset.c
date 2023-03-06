f7562b9f182bb7f79cb911727e299f3378e86a1a
Vince Rono <vkrono@gmail.com>
2023-03-06 20:28:22 +0000

    fills memory with constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 /**
  * _memset - fill a block of memory with a specific value
@@ -11,6 +10,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
+
 	for (; n > 0; i++)
 	{
 		s[i] = b;
