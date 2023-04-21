8c063fc6d6514f7b4c251ba541964858156cf707
VinceRono <vkrono@gmail.com>
2023-04-21 13:55:32 +0000

    read text file & print to POSIX standard output

---
+++ 0x15-file_io/0-reaad_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read the txt file & print to STFOUT
+ * @filename: txt file to be read
+ * @letters: no. of letters to be read
+ * Return: if write fails or does not write the 
+ * expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *bf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, bf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(bf);
+	close(fd);
+	return(w);
+}
