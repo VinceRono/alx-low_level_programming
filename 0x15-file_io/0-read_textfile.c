d0c989dee0d62f64268c8a24fec0a45258a196a3
VinceRono <vkrono@gmail.com>
2023-04-21 14:01:30 +0000

    read text file & print to POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,8 +5,7 @@
  * read_textfile - Read the txt file & print to STFOUT
  * @filename: txt file to be read
  * @letters: no. of letters to be read
- * Return: if write fails or does not write the 
- * expected amount of bytes, return 0
+ * Return: if write fails or does not the expected amount of bytes, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -18,11 +17,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
+	bf = malloc(sizeof(char) * letters);
 	t = read(fd, bf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	w = write(STDOUT_FILENO, bf, t);
 
 	free(bf);
 	close(fd);
-	return(w);
+	return (w);
 }
